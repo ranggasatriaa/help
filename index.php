8fdef941bf11abff886f075ff0a126af548d68da
Rangga Satria A <ranggasatriaa@gmail.com>
2020-01-07 23:54:35 +0700

    Init

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Help Your Self - CI</title>
+</head>
+<body>
+  <h3>This script will help you avoid repeatative task</h3>
+  <table>
+    <tbody>
+      <tr>
+        <td> <a href="input.php"> Input parameter </a></td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
---
+++ input.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>CI Input Parameter</title>
+  </head>
+  <body>
+    <form class="" action="input.php" method="post">
+      <label>Input your list of parameter</label><br>
+      <textarea name="parameter" rows="8" cols="80" placeholder="devide parameter with comma (,)"><?=empty($_POST['parameter'])?'':$_POST['parameter']?></textarea>
+      <br>
+      <input type="submit" name="submit" value="Submit">
+      <a href="index.html">Back</a>
+    </form>
+  </body>
+</html>
+
+<?php
+if (!empty($_POST['parameter'])) {
+  $input = $_POST['parameter'];
+
+  $parameters = explode(",",$input);
+  echo "<h3>Enjoy: </h3>";
+  foreach ($parameters as $param) {
+    echo "<br>$".$param." = \$this->input->post('".$param."');";
+  }
+  echo "<br>[";
+  foreach ($parameters as $param) {
+    echo "<br>'".$param."' => $".$param.",";
+  }
+  echo "<br>];";
+}
+ ?>
